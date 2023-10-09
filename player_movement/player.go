cd0053694431ee4ca4ceed349a7bfee9af856eae
Evgenii Starostin <pareylook@gmail.com>
2023-10-09 18:48:57 +0200

    simple player movement example

---
+++ player_movement/player.go
@@ -0,0 +1,35 @@
+components {
+  id: "player_movement"
+  component: "/player_movement/player_movement.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/sprites/space_shooter.atlas\"\n"
+  "default_animation: \"Kla\\\'ed - Battlecruiser - Base\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
